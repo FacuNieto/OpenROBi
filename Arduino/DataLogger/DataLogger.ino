5b34bbd1046e0f786f07b8abd66a6fbae4958724
FNieto <80937113+FacuNieto@users.noreply.github.com>
2022-07-17 06:25:30 -0300

    Update DataLogger.ino

--- Arduino/DataLogger/DataLogger.ino
+++ Arduino/DataLogger/DataLogger.ino
@@ -1,3 +1,23 @@
+// Copyright 2022 Facundo Nieto
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Este código capta la información de distintos potenciometros, acelerometros y giroscopios y los envía por puerto serie. El mismo está intencionado 
+// para la realización de una base de datos para entrenar modelos de machine learning. 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 //////////Acelerometro/////////
 #include <SPI.h>
 #include <Wire.h>
@@ -89,7 +109,9 @@ void setup() {
 }
 
 void loop() {
-
+  
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+  // Captación de valores analógicos
   entrada1 = analogRead(0);
   entrada2 = analogRead(1);
   entrada3 = analogRead(2);
@@ -97,27 +119,33 @@ void loop() {
   entrada5 = analogRead(4);
   entrada6 = analogRead(5);
 
+  // Filtrado de los valores analógicos
   Valorfiltrado1 = (alpha * entrada1) + ((1 - alpha) * Valorfiltrado1);
   Valorfiltrado2 = (alpha * entrada2) + ((1 - alpha) * Valorfiltrado2);
   Valorfiltrado3 = (alpha * entrada3) + ((1 - alpha) * Valorfiltrado3);
   Valorfiltrado4 = (alpha * entrada4) + ((1 - alpha) * Valorfiltrado4);
   Valorfiltrado5 = (alpha * entrada5) + ((1 - alpha) * Valorfiltrado5);
   Valorfiltrado6 = (alpha * entrada6) + ((1 - alpha) * Valorfiltrado6);
-
   ////////////////////////////////////////////////////////////////////////////////////////////////
-  digitalWrite(controlAc1, LOW); digitalWrite(controlAc2, HIGH); digitalWrite(controlAc3, HIGH);
+  
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+  // Captación de valores de aceleración por parte del primer acelerómetro
+  // Se selecciona utilizando la siguiente configuración de pines
+  digitalWrite(controlAc1, LOW);
+  digitalWrite(controlAc2, HIGH);
+  digitalWrite(controlAc3, HIGH);
 
   Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
-  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
-  Wire.requestFrom(MPU_ADDR, 7 * 2, true); // request a total of 7*2=14 registers
+  Wire.write(0x3B); 
+  Wire.endTransmission(false); 
+  Wire.requestFrom(MPU_ADDR, 7 * 2, true); 
 
-  accelerometer_x = Wire.read() << 8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
-  accelerometer_y = Wire.read() << 8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
-  accelerometer_z = Wire.read() << 8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
-  gyro_x = Wire.read() << 8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
-  gyro_y = Wire.read() << 8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
-  gyro_z = Wire.read() << 8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+  accelerometer_x = Wire.read() << 8 | Wire.read(); 
+  accelerometer_y = Wire.read() << 8 | Wire.read(); 
+  accelerometer_z = Wire.read() << 8 | Wire.read(); 
+  gyro_x = Wire.read() << 8 | Wire.read(); 
+  gyro_y = Wire.read() << 8 | Wire.read(); 
+  gyro_z = Wire.read() << 8 | Wire.read(); 
 
   ValorfiltradoAx1 = (alpha * accelerometer_x) + ((1 - alpha) * ValorfiltradoAx1);
   ValorfiltradoAy1 = (alpha * accelerometer_y) + ((1 - alpha) * ValorfiltradoAy1);
@@ -128,19 +156,23 @@ void loop() {
   ////////////////////////////////////////////////////////////////////////////////////////////////
 
   ////////////////////////////////////////////////////////////////////////////////////////////////
-  digitalWrite(controlAc1, HIGH); digitalWrite(controlAc2, LOW); digitalWrite(controlAc3, HIGH);
+  // Captación de valores de aceleración por parte del segundo acelerómetro
+  // Se selecciona utilizando la siguiente configuración de pines:
+  digitalWrite(controlAc1, HIGH);
+  digitalWrite(controlAc2, LOW);
+  digitalWrite(controlAc3, HIGH);
 
   Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
-  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
-  Wire.requestFrom(MPU_ADDR, 7 * 2, true); // request a total of 7*2=14 registers
+  Wire.write(0x3B); 
+  Wire.endTransmission(false); 
+  Wire.requestFrom(MPU_ADDR, 7 * 2, true); 
 
-  accelerometer_x2 = Wire.read() << 8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
-  accelerometer_y2 = Wire.read() << 8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
-  accelerometer_z2 = Wire.read() << 8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
-  gyro_x2 = Wire.read() << 8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
-  gyro_y2 = Wire.read() << 8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
-  gyro_z2 = Wire.read() << 8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+  accelerometer_x2 = Wire.read() << 8 | Wire.read(); 
+  accelerometer_y2 = Wire.read() << 8 | Wire.read(); 
+  accelerometer_z2 = Wire.read() << 8 | Wire.read(); 
+  gyro_x2 = Wire.read() << 8 | Wire.read(); 
+  gyro_y2 = Wire.read() << 8 | Wire.read(); 
+  gyro_z2 = Wire.read() << 8 | Wire.read(); 
 
   ValorfiltradoAx2 = (alpha * accelerometer_x2) + ((1 - alpha) * ValorfiltradoAx2);
   ValorfiltradoAy2 = (alpha * accelerometer_y2) + ((1 - alpha) * ValorfiltradoAy2);
@@ -151,19 +183,23 @@ void loop() {
   ////////////////////////////////////////////////////////////////////////////////////////////////
 
   ////////////////////////////////////////////////////////////////////////////////////////////////
-  digitalWrite(controlAc1, HIGH); digitalWrite(controlAc2, HIGH); digitalWrite(controlAc3, LOW);
+  // Captación de valores de aceleración por parte del tercer acelerómetro
+  // Se selecciona utilizando la siguiente configuración de pines:
+  digitalWrite(controlAc1, HIGH);
+  digitalWrite(controlAc2, HIGH);
+  digitalWrite(controlAc3, LOW);
 
   Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
-  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
-  Wire.requestFrom(MPU_ADDR, 7 * 2, true); // request a total of 7*2=14 registers
+  Wire.write(0x3B);
+  Wire.endTransmission(false); 
+  Wire.requestFrom(MPU_ADDR, 7 * 2, true);
 
-  accelerometer_x3 = Wire.read() << 8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
-  accelerometer_y3 = Wire.read() << 8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
-  accelerometer_z3 = Wire.read() << 8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
-  gyro_x3 = Wire.read() << 8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
-  gyro_y3 = Wire.read() << 8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
-  gyro_z3 = Wire.read() << 8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+  accelerometer_x3 = Wire.read() << 8 | Wire.read(); 
+  accelerometer_y3 = Wire.read() << 8 | Wire.read(); 
+  accelerometer_z3 = Wire.read() << 8 | Wire.read(); 
+  gyro_x3 = Wire.read() << 8 | Wire.read(); 
+  gyro_y3 = Wire.read() << 8 | Wire.read(); 
+  gyro_z3 = Wire.read() << 8 | Wire.read(); 
 
   ValorfiltradoAx3 = (alpha * accelerometer_x3) + ((1 - alpha) * ValorfiltradoAx3);
   ValorfiltradoAy3 = (alpha * accelerometer_y3) + ((1 - alpha) * ValorfiltradoAy3);
@@ -173,49 +209,52 @@ void loop() {
   ValorfiltradoGz3 = (alpha * gyro_z3) + ((1 - alpha) * ValorfiltradoGz3);
   ////////////////////////////////////////////////////////////////////////////////////////////////
 
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+  // En esta sección se envían los valores captados a traves de comunicación serial. Los datos son 
+  // enviados de separados por coma, en rangos de 0 a 180° separados por estos valores, solo por fines
+  // de visualización. 
   
-//  Serial.print(map(Valorfiltrado1, 0, 1023, 0, 180)); //Pote 1 delt medial
-//  Serial.print(",");
-//  Serial.print(map(Valorfiltrado2, 0, 1023, 180, 360)); //Pote 2 delt anterior
-//  Serial.print(",");
+  Serial.print(map(Valorfiltrado1, 0, 1023, 0, 180)); //Pote 1 delt medial
+  Serial.print(",");
+  Serial.print(map(Valorfiltrado2, 0, 1023, 180, 360)); //Pote 2 delt anterior
+  Serial.print(",");
   Serial.print(map(Valorfiltrado3, 430, 1000, 135, 0)); //Pote 3 biceps
   Serial.print(","); 
-//  Serial.print(map(Valorfiltrado4, 0, 1023, 540, 720)); //Pote 4 muñeca
-//  Serial.print(",");
-//  Serial.print(map(Valorfiltrado5, 0, 1023, 720, 900)); //Pote 5 metacarpofalangica
-//  Serial.print(",");
-//  Serial.print(map(Valorfiltrado6, 0, 1023, 900, 1080)); //Pote 6 falange prox-med
-//  Serial.print(",");
+  Serial.print(map(Valorfiltrado4, 0, 1023, 540, 720)); //Pote 4 muñeca
+  Serial.print(",");
+  Serial.print(map(Valorfiltrado5, 0, 1023, 720, 900)); //Pote 5 metacarpofalangica
+  Serial.print(",");
+  Serial.print(map(Valorfiltrado6, 0, 1023, 900, 1080)); //Pote 6 falange prox-med
+  Serial.print(",");
   Serial.print(map(ValorfiltradoAx1, -4800, 4800, 1080, 1260)); // Acelerometro 1 eje x
   Serial.print(",");
   Serial.print(map(ValorfiltradoAy1, -4800, 4800, 1260, 1440)); // Acelerometro 1 eje y
   Serial.print(",");
   Serial.print(map(ValorfiltradoAz1, -4800, 4800, 1440, 1620)); // Acelerometro 1 eje z
-//  Serial.print(",");
-//  Serial.print(map(ValorfiltradoAx2, -4800, 4800, 1620, 1800)); // Acelerometro 2 eje x
-//  Serial.print(",");
-//  Serial.print(map(ValorfiltradoAy2, -4800, 4800, 1800, 1980)); // Acelerometro 2 eje y
-//  Serial.print(",");
-//  Serial.print(map(ValorfiltradoAz2, -4800, 4800, 1980, 2160)); // Acelerometro 2 eje z
-//  Serial.print(",");
-//  Serial.print(map(ValorfiltradoAx3, -4800, 4800, 2160, 2340)); // Acelerometro 2 eje x
-//  Serial.print(",");
-//  Serial.print(map(ValorfiltradoAy3, -4800, 4800, 2340, 2520)); // Acelerometro 2 eje y
-//  Serial.print(",");
-//  Serial.print(map(ValorfiltradoAz3, -4800, 4800, 2520, 2700)); // Acelerometro 2 eje z
+  Serial.print(",");
+  Serial.print(map(ValorfiltradoAx2, -4800, 4800, 1620, 1800)); // Acelerometro 2 eje x
+  Serial.print(",");
+  Serial.print(map(ValorfiltradoAy2, -4800, 4800, 1800, 1980)); // Acelerometro 2 eje y
+  Serial.print(",");
+  Serial.print(map(ValorfiltradoAz2, -4800, 4800, 1980, 2160)); // Acelerometro 2 eje z
+  Serial.print(",");
+  Serial.print(map(ValorfiltradoAx3, -4800, 4800, 2160, 2340)); // Acelerometro 2 eje x
+  Serial.print(",");
+  Serial.print(map(ValorfiltradoAy3, -4800, 4800, 2340, 2520)); // Acelerometro 2 eje y
+  Serial.print(",");
+  Serial.print(map(ValorfiltradoAz3, -4800, 4800, 2520, 2700)); // Acelerometro 2 eje z
   Serial.print(",");
   Serial.print(map(ValorfiltradoGy1, -30000, 30000, 2880, 3060)); // Giroscopio 1 eje y
   Serial.print(",");
   Serial.println(map(ValorfiltradoGz1, -30000, 30000, 3060, 3240)); // Giroscopio 1 eje z
-//  Serial.print(",");
-//  Serial.print(map(ValorfiltradoGy2, -30000, 30000, 3420, 3600)); // Giroscopio 2 eje y
-//  Serial.print(",");
-//  Serial.print(map(ValorfiltradoGz2, -30000, 30000, 3600, 3780)); // Giroscopio 2 eje z
-//  Serial.print(",");
-//  Serial.println(map(ValorfiltradoGy3, -30000, 30000, 3960, 4140)); // Giroscopio 2 eje y
-////  Serial.print(",");
-////  Serial.println(map(ValorfiltradoGz3, 30000, 30000, 4140, 4320)); // Giroscopio 2 eje z
+  Serial.print(",");
+  Serial.print(map(ValorfiltradoGy2, -30000, 30000, 3420, 3600)); // Giroscopio 2 eje y
+  Serial.print(",");
+  Serial.print(map(ValorfiltradoGz2, -30000, 30000, 3600, 3780)); // Giroscopio 2 eje z
+  Serial.print(",");
+  Serial.println(map(ValorfiltradoGy3, -30000, 30000, 3960, 4140)); // Giroscopio 2 eje y
+ //  Serial.print(",");
+ //  Serial.println(map(ValorfiltradoGz3, 30000, 30000, 4140, 4320)); // Giroscopio 2 eje z
+ ////////////////////////////////////////////////////////////////////////////////////////////////
   
-
-
 }
